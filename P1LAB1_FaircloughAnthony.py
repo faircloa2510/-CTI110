5e5b7f62a27ac9c11e83f3a0f4d2a2d1f38e2fbd
faircloa2510 <166546075+faircloa2510@users.noreply.github.com>
2024-04-09 14:13:10 -0400

    Add files via upload

---
+++ P1LAB1_FaircloughAnthony.py
@@ -0,0 +1,8 @@
+#Anthony Fairclough
+#April 9, 2024
+#P1LAB1 Instructions
+#This assignment teaches the student how to create a Python program using IDLE. It shows how to get input from the user and display output.
+
+firstname = input("Enter your first name please:")
+lastname = input("Enter your last please name:")
+print ("Hello", firstname,lastname,"Welcome to CTI-110")
